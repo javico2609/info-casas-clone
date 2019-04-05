60242f781b52bcdc3d59fa6c692864a72f0de334
Javier <javico2609@gmail.com>
2019-04-05 16:19:36 -0300

    chore: add home page

---
+++ lib/src/pages/pages.dart
@@ -0,0 +1,2 @@
+export 'package:info_casas_clone/src/pages/home.dart';
+export 'package:info_casas_clone/src/pages/sign-in.dart';
---
+++ lib/src/pages/sign-in.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:info_casas_clone/src/pages/pages.dart';
+import 'package:info_casas_clone/theme/theme.dart';
+
+class SignInPage extends StatelessWidget {
+
+  _goToHomePage(BuildContext context) {
+     Navigator.of(context).pushReplacement(
+       MaterialPageRoute(
+         builder: (context) => new HomePage()
+       )
+     );
+  }
+
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("InfoCasas"),
+        leading: IconButton(
+          onPressed: () => _goToHomePage(context),
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 70.0,
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Iniciar Sesión",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: textColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Form(
+                  autovalidate: false,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          hintText: 'Email',
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextFormField(
+                        obscureText: true,
+                        textInputAction: TextInputAction.done,
+                        decoration: InputDecoration(
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: textColor),
+                          ),
+                          hintText: 'Password',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  alignment: Alignment.centerRight,
+                  height: 30,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "olvidé mi contraseña",
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  height: 45,
+                  width: double.infinity,
+                  child: RaisedButton(
+                    child: Text(
+                      "INGRESAR",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                    color: textColor,
+                    onPressed: () {},
+                  ),
+                ),
+                SizedBox(height: 30),
+                Divider(color: Colors.black),
+                SizedBox(height: 30),
+                Text(
+                  "o ingresar con",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 17, color: Colors.grey[500]),
+                ),
+                SizedBox(height: 15),
+                Card(
+                  elevation: 5,
+                  color: fbColor,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 40,
+                      width: double.infinity,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.centerLeft,
+                            width: 40,
+                            child: Icon(
+                              Icons.face,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Ingresar con Facebook",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Card(
+                  elevation: 5,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 40,
+                      width: double.infinity,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.centerLeft,
+                            width: 40,
+                            child: Icon(
+                              Icons.group_work,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Ingresar con Google",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.grey[700],
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Aún no tienes cuenta ?",
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 17,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Registrate",
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: textColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 25),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    "Omitir",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
